c80cd66267cb500f357e6902cd6ec287157c3bdb
Candoso <pedrobcandoso@gmail.com>
2023-02-05 19:56:49 +0000

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -14,8 +14,15 @@ project "GLFW"
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+
+        "src/platform.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
     }
 
 	filter "system:windows"
@@ -26,15 +33,15 @@ project "GLFW"
         files
         {
             "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
-        }
+			"src/win32_joystick.c",
+			"src/win32_module.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"        }
 
 		defines 
 		{ 
